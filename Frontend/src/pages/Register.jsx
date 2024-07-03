1bc7876c3bbb31fb9966f38927286f4356fb6e41
Dario Lo Forti <lofortidario@gmail.com>
2024-07-04 00:41:56 +0200

    Update registration form layout and styles

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -35,60 +35,73 @@ export default function(){
     }
 
     return (
-        
-        <div className="register-container">
-                <h1>Register</h1>
-            <form onSubmit={handleSubmit} >
-                    <label>Name</label>
-                    <input 
-                        type="text"
-                        placeholder="Username" 
-                        required
-                        value={formData.name}
-                        onChange={e => changeData('name', e.target.value)}/>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="register-container">
+                            <h1 className="text-center mb-5">Register</h1>
+                        <form onSubmit={handleSubmit} >
+                            <div className="top-form">
+                                <label>Name</label>
+                                <input 
+                                    type="text"
+                                    placeholder="Username" 
+                                    required
+                                    value={formData.name}
+                                    onChange={e => changeData('name', e.target.value)}/>
 
-                    <label>Surname</label>
-                    <input 
-                        type="text"
-                        placeholder="Surname" 
-                        required
-                        value={formData.surname}
-                        onChange={e => changeData('surname', e.target.value)}/>
+                                <label>Surname</label>
+                                <input 
+                                    type="text"
+                                    placeholder="Surname" 
+                                    required
+                                    value={formData.surname}
+                                    onChange={e => changeData('surname', e.target.value)}/>
 
-                    <label>Phone</label>
-                    <input 
-                        type="text"
-                        placeholder="Phone" 
-                        required
-                        value={formData.phone}
-                        onChange={e => changeData('phone', e.target.value)}/>
-                    <label>Email</label>
-                    <input 
-                        type="text"
-                        placeholder="Email" 
-                        required
-                        value={formData.email}
-                        onChange={e => changeData('email', e.target.value)}/>
-                
-                    <label>Password</label>
-                    <input 
-                        type="password"
-                        required
-                        placeholder="Password" 
-                        value={formData.password}
-                        onChange={e => changeData('password', e.target.value)}/>
-                
-                    <label>Image Profile</label>
-                    <input 
-                    type="file"
-                    onChange={e => changeData('imageUrl', e.target.files[0])}/>
-            
-                {signupError !== null && <div className="error">{signupError.message}</div>}
-                {signupError?.errors && signupError.errors.map( (err, index) => (
-                    <div key={`err${index}`}>{err.msg}</div>
-                ))}
-                <button>Register</button>
-            </form>
+                                    <label>Phone</label>
+                                <input 
+                                    type="text"
+                                    placeholder="Phone" 
+                                    required
+                                    value={formData.phone}
+                                    onChange={e => changeData('phone', e.target.value)}/>
+                            </div>
+                            <div className="main-form">
+                            <label>Email</label>
+                                <input 
+                                    type="text"
+                                    placeholder="Email" 
+                                    required
+                                    value={formData.email}
+                                    onChange={e => changeData('email', e.target.value)}/>
+                            
+                                <label>Password</label>
+                                <input 
+                                    type="password"
+                                    required
+                                    placeholder="Password" 
+                                    value={formData.password}
+                                    onChange={e => changeData('password', e.target.value)}/>
+                            
+                               
+                            </div>
+                            <div className="buttom-form">
+                            <label>Image Profile</label>
+                                <input 
+                                type="file"
+                                onChange={e => changeData('imageUrl', e.target.files[0])}/>
+                            </div>
+                            
+                            {signupError !== null && <div className="error">{signupError.message}</div>}
+                            {signupError?.errors && signupError.errors.map( (err, index) => (
+                                <div key={`err${index}`}>{err.msg}</div>
+                            ))}
+                            <button>Register</button>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
